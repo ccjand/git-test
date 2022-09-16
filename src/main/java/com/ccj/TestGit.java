0a5b2399b80eaea8e3654ffda0cfc97b46890892
ccj <3177587750@qq.com>
2022-09-16 22:20:04 +0800

    second commit

--- src/main/java/com/ccj/TestGit.java
+++ src/main/java/com/ccj/TestGit.java
@@ -3,6 +3,6 @@ package com.ccj;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
-
+        System.out.println("come on ccj!!!!");
     }
 }
