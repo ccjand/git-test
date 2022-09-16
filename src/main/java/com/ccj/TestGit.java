0cbf0e77fec3a454902f4249d8df761e27bff457
ccj <3177587750@qq.com>
2022-09-16 22:29:22 +0800

    third commit

--- src/main/java/com/ccj/TestGit.java
+++ src/main/java/com/ccj/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("come on ccj!!!!");
+        System.out.println("go go go go!!!");
     }
 }
